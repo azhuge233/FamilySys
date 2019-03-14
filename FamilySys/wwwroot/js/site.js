5acadb9a417e6d3cb62bf9bf60089edefe2764fb
233 azhuge <azhuge786698994@gmail.com>
2019-03-14 20:21:53 +0800

    Minify static assets

--- FamilySys/wwwroot/js/site.js
+++ FamilySys/wwwroot/js/site.js
@@ -4,52 +4,52 @@
 // Write your JavaScript code.
 
 function getCookie(name) {
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg))
-        return arr[2];
-    else 
-        return null;
+	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+	if (arr = document.cookie.match(reg))
+		return arr[2];
+	else
+		return null;
 }
 
 function setCookie(name, value) {
-    var Days = 1;
-    var exp = new Date();
-    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-    document.cookie = name + "=" + value + ";expires=" + exp.toGMTString();
+	var Days = 1;
+	var exp = new Date();
+	exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+	document.cookie = name + "=" + value + ";expires=" + exp.toGMTString();
 }
 
 function changeColor(btn, body, mode) {
 	if (mode == "night") {
-		btn.style.setProperty('background-color', '#000');
-		btn.style.setProperty('color', '#fff');
+		btn.style.setProperty("background-color", "#000");
+		btn.style.setProperty("color", "#fff");
 		btn.innerHTML = "夜间";
 
-		body.style.setProperty('background-color', '#333');
-		body.style.setProperty('color', '#F5F5F5');
+		body.style.setProperty("background-color", "#333");
+		body.style.setProperty("color", "#F5F5F5");
 	} else {
-		btn.style.setProperty('background-color', '#d2691e');
-		btn.style.setProperty('color', '#e0ffff');
+		btn.style.setProperty("background-color", "#d2691e");
+		btn.style.setProperty("color", "#e0ffff");
 		btn.innerHTML = "日间";
 
-		body.style.setProperty('background-color', '#fff');
-		body.style.setProperty('color', '#333');
+		body.style.setProperty("background-color", "#fff");
+		body.style.setProperty("color", "#333");
 	}
 }
 
 function changeMode(thisID) {
-		var btn = document.getElementById(thisID);
-		var body = document.body;
-		if (btn.innerHTML == "日间") {
-			changeColor(btn, body, "night");
-			setCookie('mode', 'night');
-		} else {
-			changeColor(btn, body, "day");
-			setCookie('mode', 'sun');
-		}
+	var btn = document.getElementById(thisID);
+	var body = document.body;
+	if (btn.innerHTML == "日间") {
+		changeColor(btn, body, "night");
+		setCookie("mode", "night");
+	} else {
+		changeColor(btn, body, "day");
+		setCookie("mode", "sun");
+	}
 }
-	
+
 window.onload = function() {
-}
+};
 
 function ChgTextBoxStats(usernameID, sexID, phoneID, mailID, btnID, thisbtnID) {
 	var thisBtn = document.getElementById(thisbtnID);
